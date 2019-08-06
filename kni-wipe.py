b545505d32517e7ed26fb9dcfe2db94c1536c35e
Benjamin Schmaus <bschmaus@redhat.com>
2019-08-06 14:54:57 -0500

    Update kni-wipe.py

--- kni-wipe.py
+++ kni-wipe.py
@@ -325,7 +325,7 @@ if __name__ == "__main__":
     if vendor_type == "DELL":
         check_supported_idrac_version()
         get_storage_controllers()
-   	create_raid_vd()
+        create_raid_vd()
    	if job_type == "realtime":
             loop_job_status()
         elif job_type == "staged":
